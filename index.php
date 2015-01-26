825478edbba50a17f0611f280361b13b0c5144dd
Matthew McVickar <matthew@matthewmcvickar.com>
2015-01-26 15:21:29 -0800

    Use test data for local development.

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 require_once('api-settings.php');
 
-$books = simplexml_load_file('https://www.goodreads.com/review/list?format=xml&v=2&id=3519627&shelf=read&key=' . API_KEY);
+// Query the Goodreads API.
+// $books = simplexml_load_file('https://www.goodreads.com/review/list?format=xml&v=2&id=3519627&shelf=read&key=' . API_KEY);
+
+// Read local copy while developing.
+$books = simplexml_load_file('test-data.xml');
 
 echo "<pre>";
 print_r($books);
